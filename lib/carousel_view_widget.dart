0728f8a0d089066a1a9b54ee923733152066577d
tarik <mdtarikbinaziz@gmail.com>
2025-04-24 13:25:56 +0600

    add caroselview

--- lib/carousel_view_widget.dart
+++ lib/carousel_view_widget.dart
@@ -11,6 +11,7 @@ Full-screen: This layout shows one edge-to-edge large item at a time and scrolls
 */
 
 import 'package:flutter/material.dart';
+import 'package:flutter_widgets_list/componants/images.dart';
 
 class CarouselViewWidget extends StatefulWidget {
   const CarouselViewWidget({super.key});
@@ -20,13 +21,80 @@ class CarouselViewWidget extends StatefulWidget {
 }
 
 class _CarouselViewWidgetState extends State<CarouselViewWidget> {
+  final CarouselController controller = CarouselController(initialItem: 1);
   @override
   Widget build(BuildContext context) {
+    final double height = MediaQuery.sizeOf(context).height;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Carousel View'),
       ),
-      body: Container(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ConstrainedBox(
+              constraints: BoxConstraints(maxHeight: height / 2.5),
+              child: CarouselView(
+                scrollDirection: Axis.horizontal,
+                itemExtent: 300,
+                shrinkExtent: 80,
+                itemSnapping: true,
+                children: List<Widget>.generate(imageList.length, (int index) {
+                  return Image.asset(
+                    height: 200,
+                    imageList[index],
+                    fit: BoxFit.cover,
+                  );
+                }),
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Padding(
+              padding: EdgeInsetsDirectional.only(top: 8.0, start: 8.0),
+              child: Text('Multi-browse layout'),
+            ),
+            const SizedBox(height: 20),
+            ConstrainedBox(
+              constraints: BoxConstraints(maxHeight: height / 2.5),
+              child: CarouselView.weighted(
+                scrollDirection: Axis.horizontal,
+                flexWeights: const <int>[1, 2, 3, 2, 1],
+                consumeMaxWeight: false,
+                children: List<Widget>.generate(imageList.length, (int index) {
+                  return Image.asset(
+                    height: 200,
+                    imageList[index],
+                    fit: BoxFit.cover,
+                  );
+                }),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ConstrainedBox(
+              constraints: BoxConstraints(maxHeight: height / 2.5),
+              child: CarouselView.weighted(
+                controller: controller,
+                scrollDirection: Axis.horizontal,
+                flexWeights: const <int>[
+                  1,
+                  7,
+                  1,
+                ],
+                itemSnapping: true,
+                children: List<Widget>.generate(imageList.length, (int index) {
+                  return Image.asset(
+                    height: 200,
+                    imageList[index],
+                    fit: BoxFit.cover,
+                  );
+                }),
+              ),
+            ),
+            const SizedBox(height: 20),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/componants/images.dart
@@ -0,0 +1,8 @@
+var imageList = [
+  "assets/images/SCR-20250424-klos.png",
+  "assets/images/SCR-20250424-klri.png",
+  "assets/images/SCR-20250424-kluj.png",
+  "assets/images/SCR-20250424-klxj.png",
+  "assets/images/SCR-20250424-klzc.png",
+  "assets/images/SCR-20250424-kmbb.png"
+];
